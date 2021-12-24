d981d773cfdfdf62bc4fcd5bfa61990e2be9a8b9
iriska <thebluemitsu@gmail.com>
2021-12-25 01:34:53 +0300

    save

--- PHILO/inc/philo.h
+++ PHILO/inc/philo.h
@@ -1,6 +1,15 @@
 #ifndef PHILO_H
 # define PHILO_H
 # include <stdio.h>
+# include <pthread.h> 
+# include <stdlib.h>
 
+typedef struct s_needed
+{
+	int arguments[6];
+	pthread_mutex_t fork[2];
+	int eating;
+	int sleeping;
+}	t_needed;
 
 #endif
--- PHILO/src/philo.c
+++ PHILO/src/philo.c
@@ -1,6 +1,42 @@
 #include "../inc/philo.h"
+#include <pthread.h>
 
-int main()
+t_needed life_init(int argc, char *argv[])
 {
-	printf("hello there.\n");
+	t_needed sample;
+	int i;
+
+	i = 1;
+	while(--argc)
+	{	
+		sample.arguments[i - 1] = atoi(argv[i]); // careful !!! not my atoi
+		i++;
+	}
+	sample.arguments[i - 1] = -1;
+	pthread_mutex_init(&sample.fork[0], NULL);
+	pthread_mutex_init(&sample.fork[1], NULL);
+	sample.sleeping = 0;
+	sample.eating = 0;
+	return (sample);
+}
+
+void *test(void *args)
+{
+	t_needed *life;
+
+	life = args;
+	printf("I'm inside the thread, sleeping is %d\n", life->sleeping);
+	life->sleeping++;
+	// free args?
+	return(NULL);
+}
+
+int main(int argc, char *argv[])
+{
+	pthread_t id[5];
+	t_needed life = life_init(argc, argv);
+	pthread_create(&id[0], NULL, &test, &life);
+	pthread_join(id[0], NULL);
+	printf("I'm outside the thread, sleeping is %d\n", life.sleeping);
+	return (0);
 }
