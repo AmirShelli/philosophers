00e90893ad7128100564ea88ddb8a539f06d86b4
Boyd Harghaz <bharghaz@pl-a1.msk.21-school.ru>
2021-12-29 21:22:02 +0300

    save

--- PHILO/src/philo.c
+++ PHILO/src/philo.c
@@ -2,22 +2,22 @@
 #include <pthread.h>
 #include <stdio.h>
 
-t_needed life_init(int argc, char *argv[])
+t_needed *life_init(int argc, char *argv[], int philosopher)
 {
-	t_needed sample;
+	t_needed *sample;
 	int i;
 
 	i = 1;
+	sample = malloc(sizeof(t_needed));
 	while(--argc)
 	{	
-		sample.arguments[i - 1] = atoi(argv[i]); // careful !!! not my atoi
+		sample->arguments[i - 1] = atoi(argv[i]); // careful !!! not my atoi
 		i++;
 	}
-	sample.arguments[i - 1] = -1;
-	pthread_mutex_init(&sample.fork[0], NULL);
-	pthread_mutex_init(&sample.fork[1], NULL);
-	sample.sleeping = 0;
-	sample.eating = 0;
+	sample->philosopher = philosopher;
+	sample->arguments[i - 1] = -1;
+	sample->sleeping = 0;
+	sample->eating = 0;
 	return (sample);
 }
 
@@ -25,16 +25,18 @@ void *test(void *args)
 {
 	t_needed *life;
 
+
 	life = args;
-	pthread_mutex_lock(&life->fork[0]);
-	printf("philosopher %d is picking" "\033[0;31m" " first fork" "\033[0m" ".\n", life->philosopher);
+	if(!pthread_mutex_lock(&life->fork[0]))
+	{printf("philosopher %d is picking" "\033[0;31m" " first fork" "\033[0m" ".\n", life->philosopher);
 	pthread_mutex_lock(&life->fork[1]);
 	printf("philosopher %d is picking" "\033[0;31m" " second fork" "\033[0m" ".\n", life->philosopher);
 	printf("philosopher %d is" "\033[0;32m" " eating" "\033[0m" ".\n", life->philosopher);
 	// while() count eating, make a funct?
 	pthread_mutex_unlock(&life->fork[0]);
-	pthread_mutex_unlock(&life->fork[1]);
+	pthread_mutex_unlock(&life->fork[1]);}
 	// free args?
+	free(args);
 	return(NULL);
 }
 
@@ -43,18 +45,19 @@ int main(int argc, char *argv[])
 	int	*args;
 	pthread_t id[6];
 	int philo_count;
-	int check_death = 3;
-	t_needed life = life_init(argc, argv);
-	
+	int check_death = 1;
+	t_needed *life; 
+
 	args = parsed(argc, argv); // don't forget to free.
 	while(check_death--)
 	{	
 		philo_count = 0;
 		while(++philo_count < 6)
-		{	printf("supposed to be philo %d\n", philo_count);
-			life.philosopher = philo_count;
-			pthread_create(&id[philo_count - 1], NULL, &test, &life);
-			// pthread_join(id[philo_count - 1], NULL);
+		{	
+			life = life_init(argc, argv, philo_count);
+			pthread_mutex_init(&life->fork[0], NULL);
+			pthread_mutex_init(&life->fork[1], NULL);
+			pthread_create(&id[philo_count - 1], NULL, &test, life);
 		}
 
 		philo_count = 0;
